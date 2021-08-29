64d01e2e44625da52c903a29733c6a34e39fbbfc
Marc Mallet <marc.mallet@utas.edu.au>
2021-08-29 21:15:28 +1000

    fix engine packages installations

--- R/install_required_packages.R
+++ R/install_required_packages.R
@@ -1,4 +1,5 @@
 list.of.packages <- c("tidyverse", "tidymodels", "vip", "janitor", "palmerpenguins",
-                      "lubridate", "fastshap", "tictoc", "ggcorrplot", "ranger")
+                      "lubridate", "fastshap", "tictoc", "ggcorrplot", 
+                      "ranger", "xgboost", "earth", "kernlab")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
